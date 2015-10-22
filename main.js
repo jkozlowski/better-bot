136b70ae2910347fdeb69e141a23e48e1b1f0278
Jakub Kozlowski <mail@jakub-kozlowski.com>
2015-10-22 17:47:00 +0100

    Build in Amazon Linux docker container. * Supported by AWS Lambda. * Artifacts zipped and pushed to S3. * All access keys encrypted.

---
+++ main.js
@@ -0,0 +1,29 @@
+var child_process = require('child_process');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+exports.handler = function(event, context) {
+
+  s3.getObject({Bucket: "better-bot", Key: "config/config.yaml"}, function(err, data) {
+        if (err) {
+            console.log("Error reading the config from S3");
+            context.fail ("Error getting file: " + err)
+        } else {
+
+          var proc = child_process.spawn('./better-bot', [ JSON.stringify(event) ], { stdio: 'inherit' });
+
+          proc.on('close', function(code) {
+            if(code !== 0) {
+              return context.done(new Error("Process exited with non-zero status code"));
+            }
+
+            context.done(null);
+          });
+
+          // console.log('Data :', );
+          // context.succeed();
+        }
+    });
+
+  //
+}
