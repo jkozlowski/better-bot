9d319beb59a8999896baeb3fe26a78b731a7df5e
Jakub Kozlowski <mail@jakub-kozlowski.com>
2015-10-23 19:14:47 +0100

    try this as well

--- cp_support_shared_libs.sh
+++ cp_support_shared_libs.sh
@@ -15,4 +15,4 @@ done
 # Works with 32 and 64 bit ld-linux
 sldl="$(ldd $pFILE | grep 'ld-linux' | awk '{ print $1}')"
 sldlsubdir="${sldl%/*}"
-[ ! -f ${d}${sldl} ] && ${_cp} -f ${sldl} ${d}${sldlsubdir}
+cp -f ${sldl} ${d}${sldlsubdir}
