9af32e188d58938897fd6ade23344f538cfc6858
Jakub Kozlowski <mail@jakub-kozlowski.com>
2015-10-23 20:25:15 +0100

    Pipe config from s3 to ./better-bot-exe.

--- index.js
+++ index.js
@@ -5,29 +5,22 @@ var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
 
-  s3.getObject({Bucket: "better-bot", Key: "config/config.yaml"}, function(err, data) {
-        if (err) {
-            console.log("Error reading the config from S3");
-            context.fail ("Error getting file: " + err)
-        } else {
+  var configStream = s3.getObject({Bucket: "better-bot", Key: "config/config.yaml"})
+                       .createReadStream();
 
-          var options = {
-            stdio: 'inherit'
-          , env: {
-              "LD_LIBRARY_PATH": "."
-          }
-          };
-          var proc = child_process.spawn('./better-bot-exe', [], options);
+  var options = {
+      stdio: ['pipe', 1, 2]
+    , env: { "LD_LIBRARY_PATH": "." }
+  };
 
-          proc.on('close', function(code) {
-            if(code !== 0) {
-              return context.done(new Error("Process exited with non-zero status code"));
-            }
+  var child = child_process.spawn('./better-bot-exe', [], options);
+  configStream.pipe(child.stdin);
 
-            context.done(null);
-          });
-        }
-    });
+  child.on('close', function(code) {
+    if(code !== 0) {
+      return context.done(new Error("Process exited with non-zero status code"));
+    }
 
-  //
+    context.done(null);
+  });
 }
