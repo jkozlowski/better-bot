e9b709e6f51d62b010de49bbf389c5bbe4ae9a10
Jakub Kozlowski <mail@jakub-kozlowski.com>
2015-10-23 19:18:01 +0100

    And this.

--- cp_support_shared_libs.sh
+++ cp_support_shared_libs.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
-d="$1"            # JAIL ROOT
-pFILE="$2"        # copy bin file libs
-files=""
-## use ldd to get shared libs list ###
-files="$(ldd $pFILE |  awk '{ print $3 }' | sed  '/^$/d')"
+# Author : Hemanth.HM
+# Email : hemanth[dot]hm[at]gmail[dot]com
+# License : GNU GPLv3
+#
 
-for i in $files
+function useage()
+{
+    cat << EOU
+Useage: bash $0 <path to the binary> <path to copy the dependencies>
+EOU
+exit 1
+}
+
+#Validate the inputs
+[[ $# < 2 ]] && useage
+
+#Check if the paths are vaild
+[[ ! -e $1 ]] && echo "Not a vaild input $1" && exit 1
+[[ -d $2 ]] || echo "No such directory $2 creating..."&& mkdir -p "$2"
+
+#Get the library dependencies
+echo "Collecting the shared library dependencies for $1..."
+deps=$(ldd $1 | awk 'BEGIN{ORS=" "}$1\
+~/^\//{print $1}$3~/^\//{print $3}'\
+ | sed 's/,$/\n/')
+echo "Copying the dependencies to $2"
+
+#Copy the deps
+for dep in $deps
 do
-  dcc="${i%/*}" # get dirname only
-  [ ! -d ${d}${dcc} ] && mkdir -p ${d}${dcc}
-  cp -f "$i" "${d}${dcc}"
+    echo "Copying $dep to $2"
+    cp "$dep" "$2"
 done
 
-# Works with 32 and 64 bit ld-linux
-sldl="$(ldd $pFILE | grep 'ld-linux' | awk '{ print $1}')"
-sldlsubdir="${sldl%/*}"
-cp -f ${sldl} ${d}${sldlsubdir}
+echo "Done!"
