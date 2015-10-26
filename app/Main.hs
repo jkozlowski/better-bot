79f611de6472d609524ccadf57d8698861768f9a
Jakub Kozlowski <mail@jakub-kozlowski.com>
2015-10-26 22:49:37 +0000

    Improve logging (add timestamps).

--- app/Main.hs
+++ app/Main.hs
@@ -22,36 +22,45 @@ import           Control.Monad
 import           Control.Monad.Catch         (MonadCatch, MonadThrow, catch)
 import           Control.Monad.IO.Class      (MonadIO, liftIO)
 import           Control.Monad.Logger
+import qualified Data.ByteString.Char8       as S8
+import           Data.Char                   (toLower)
 import           Data.List                   (concatMap)
 import           Data.List.Lens
 import qualified Data.Map.Strict             as Map
 import           Data.Monoid                 ((<>))
 import           Data.Text                   (Text, pack)
+import qualified Data.Text                   as T
+import qualified Data.Text.Encoding          as T
+import qualified Data.Text.Encoding.Error    as T
+import qualified Data.Text.IO                as T
 import qualified Data.Time.Calendar          as Time
 import qualified Data.Time.Calendar.WeekDate as Time
 import qualified Data.Time.Clock             as Time
+import qualified Data.Time.Format            as Time
+import           Language.Haskell.TH.Syntax
 import           Network.Better.Session
 import           Network.Better.Types
 import qualified Network.Wreq.Session        as S
 import           Options.Applicative         (execParser)
 import           System.Exit                 (exitFailure, exitSuccess)
+import           System.IO                   (Handle, stdout)
+import           System.Log.FastLogger       (fromLogStr)
 import qualified Text.Show.Pretty            as Pretty
 import           Types                       (BookingConfig, Config, DayOfWeek,
                                               Opts (..), bookingConfig,
                                               bookingConfigConfig,
                                               bookingConfigSlots, configEmail,
                                               configPassword,
-                                              optionsConfigFile,
                                               dayOfWeekFromUTCTime,
-                                              optsParserInfo, readConfig,
-                                              _Password, _Slots)
+                                              optionsConfigFile, optsParserInfo,
+                                              readConfig, _Password, _Slots)
 
 main :: IO ()
 main = execParser optsParserInfo >>= \opts -> do
   maybeConfig <- readConfig (opts ^. optionsConfigFile)
   case maybeConfig of
     Left e  -> exitWithError e
-    Right c -> runStdoutLoggingT $ do
+    Right c -> runAppLogging $ do
       $(logInfo) $ "Running with config: " <> (pack . Pretty.ppShow $ c)
       case opts of
         Opts (Just date) _ -> withParticularDay c date
@@ -104,3 +113,42 @@ findSlots day = concatMap go
   where go bc = case Map.lookup day (bc ^. bookingConfigSlots . _Slots) of
                   Just slot -> [(bc ^. bookingConfigConfig, slot)]
                   Nothing   -> []
+
+-- Logging setup
+
+runAppLogging :: MonadIO m => LoggingT m a -> m a
+runAppLogging = (`runLoggingT` loggerFunc)
+
+-- | Logging function takes the log level into account - stolen from stack.
+loggerFunc :: (MonadIO m, ToLogStr msg)
+           => Loc -> Text -> LogLevel -> msg -> m ()
+loggerFunc loc _src level msg =
+  liftIO (getOutput >>= T.hPutStrLn outputChannel)
+  where outputChannel = stdout
+        getOutput =
+          do date <- getDate
+             l <- getLevel
+             lc <- getLoc
+             return (T.pack date <> T.pack l <> T.decodeUtf8 (fromLogStr (toLogStr msg)) <> T.pack lc)
+          where getDate =
+                  do now <- Time.getCurrentTime
+                     return (Time.formatTime Time.defaultTimeLocale "%Y-%m-%d %T%Q" now ++
+                             ": ")
+                getLevel =
+                    return ("[" ++
+                            map toLower (drop 5 (show level)) ++
+                            "] ")
+                getLoc =
+                    return (" @(" ++ fileLocStr ++ ")")
+                fileLocStr =
+                  loc_package loc ++
+                  ':' :
+                  loc_module loc ++
+                  ' ' :
+                  loc_filename loc ++
+                  ':' :
+                  line loc ++
+                  ':' :
+                  char loc
+                  where line = show . fst . loc_start
+                        char = show . snd . loc_start
